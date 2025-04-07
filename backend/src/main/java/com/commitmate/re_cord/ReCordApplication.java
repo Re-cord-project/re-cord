e72c30ba3403381054cd2e1c548d691b6724dd36
codefish <codewithfish@gmail.com>
2025-04-07 17:58:50 +0900

    init

---
+++ backend/src/main/java/com/commitmate/re_cord/ReCordApplication.java
@@ -0,0 +1,13 @@
+package com.commitmate.re_cord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReCordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReCordApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/commitmate/re_cord/ReCordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commitmate.re_cord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReCordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
