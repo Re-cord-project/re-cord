47e5d5f457926576842c364f8273f8b9b4a870dc
codefish <codewithfish@gmail.com>
2025-04-10 14:53:55 +0900

    chore:ai test

--- backend/src/main/java/com/commitmate/re_cord/domain/post/post/controller/ApiV1PostController.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/post/post/controller/ApiV1PostController.java
@@ -1,9 +1,19 @@
 package com.commitmate.re_cord.domain.post.post.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ApiV1PostController {
+    @PostMapping
+    public String index() {
+        return "Hello World!";
+    }
+
+    @PostMapping
+    public String create(){
+        return "Post Created";
+    }
 }
