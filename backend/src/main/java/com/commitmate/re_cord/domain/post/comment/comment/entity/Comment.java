d3f89aebcde2efa3e8843ef2f5099e9dd8d45ee9
codefish <codewithfish@gmail.com>
2025-04-10 11:56:49 +0900

    feat:comment, category entity

--- backend/src/main/java/com/commitmate/re_cord/domain/post/comment/comment/entity/Comment.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/post/comment/comment/entity/Comment.java
@@ -1,4 +1,37 @@
 package com.commitmate.re_cord.domain.post.comment.comment.entity;
 
-public class Comment {
+import com.commitmate.re_cord.domain.post.post.entity.Post;
+import com.commitmate.re_cord.domain.user.user.entity.User;
+import com.commitmate.re_cord.global.jpa.BaseEntity;
+import com.commitmate.re_cord.global.jpa.UpdateStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(exclude ={"userId", "postId"})
+public class Comment extends BaseEntity {
+
+    private int likes;
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private UpdateStatus updateStatus = UpdateStatus.NOT_EDITED;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Post postId;
+
 }
--- backend/src/main/java/com/commitmate/re_cord/domain/post/comment/commentVote/entity/CommentVote.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/post/comment/commentVote/entity/CommentVote.java
@@ -1,4 +1,26 @@
 package com.commitmate.re_cord.domain.post.comment.commentVote.entity;
 
-public class CommentVote {
+import com.commitmate.re_cord.domain.post.comment.comment.entity.Comment;
+import com.commitmate.re_cord.domain.user.user.entity.User;
+import com.commitmate.re_cord.global.jpa.BaseEntity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.ToString;
+
+@ToString(exclude ={"userId", "commentId"})
+
+public class CommentVote extends BaseEntity {
+    public boolean commentLike;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User userId;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment commentId;
+
+
 }
