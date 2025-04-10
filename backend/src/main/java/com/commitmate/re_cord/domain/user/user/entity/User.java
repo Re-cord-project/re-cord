21434efdae013c18e0dfc2aba0da00dc8f8474dc
kunhr <kunhr@naver.com>
2025-04-10 10:47:04 +0900

    feat: user 엔티티 생성

--- backend/src/main/java/com/commitmate/re_cord/domain/user/user/entity/User.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/user/user/entity/User.java
@@ -1,12 +1,21 @@
 package com.commitmate.re_cord.domain.user.user.entity;
 
+import com.commitmate.re_cord.domain.user.follow.entity.Follow;
+import com.commitmate.re_cord.domain.user.user.enums.Provider;
+import com.commitmate.re_cord.domain.user.user.enums.Role;
 import com.commitmate.re_cord.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -16,7 +25,25 @@ import lombok.experimental.SuperBuilder;
 @ToString
 
 public class User extends BaseEntity {
-    private String nickname;
     private String email;
+    private String username;
+    private String password;
+    private String bootcamp;
+    private int generation;
+    @Enumerated(EnumType.STRING)
+    private Provider provider;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
+    @OneToMany(mappedBy = "followerId")
+    private List<Follow> followingList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "followingId")
+    private List<Follow> followerList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "blockedId")
+    private List<Follow> blockingList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "blockingId")
+    private List<Follow> blockedList = new ArrayList<>();
 }
