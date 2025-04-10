ff55e5b13d22b0a2dc0a5748aa9366056bb32ee0
fism <fism@naver.com>
2025-04-10 11:11:35 +0900

    feat: 게시글 엔티티 생성 #4

--- backend/src/main/java/com/commitmate/re_cord/domain/post/post/entity/Post.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/post/post/entity/Post.java
@@ -1,7 +1,7 @@
 package com.commitmate.re_cord.domain.post.post.entity;
 
 import com.commitmate.re_cord.global.jpa.BaseEntity;
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -16,7 +16,43 @@ import java.time.LocalDateTime;
 @ToString
 public class Post extends BaseEntity {
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String content;
 
+    private int views = 0;
+    private int likes = 0;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PostStatus status;
+
+    @Enumerated(EnumType.STRING)
+    protected UpdateStatus updatedStatus;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+        this.updatedStatus = UpdateStatus.CREATED;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+        this.updatedStatus = UpdateStatus.UPDATED;
+    }
 }
