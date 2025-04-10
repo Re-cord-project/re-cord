fb6ae7a1f088b3b34c8bc6de5678ebc17e6b8374
kunhr <kunhr@naver.com>
2025-04-10 10:47:04 +0900

    feat: user 엔티티 생성

--- backend/src/main/java/com/commitmate/re_cord/domain/user/follow/entity/Follow.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/user/follow/entity/Follow.java
@@ -1,4 +1,29 @@
 package com.commitmate.re_cord.domain.user.follow.entity;
 
-public class Follow {
+import com.commitmate.re_cord.domain.user.user.entity.User;
+import com.commitmate.re_cord.global.jpa.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString
+public class Follow extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "follower_id")
+    private User followerId;
+
+    @ManyToOne
+    @JoinColumn(name = "following_id")
+    private User followingId;
 }
