46bfe615509dff8ed853f23c5241facf498d7713
codefish <codewithfish@gmail.com>
2025-04-11 16:59:57 +0900

    feat:내 게시글/댓글 불러오기

---
+++ backend/src/main/java/com/commitmate/re_cord/domain/mypage/MypageController.java
@@ -0,0 +1,38 @@
+package com.commitmate.re_cord.domain.mypage;
+
+import com.commitmate.re_cord.domain.post.comment.comment.Service.CommentService;
+import com.commitmate.re_cord.domain.post.comment.comment.dto.CommentDTO;
+import com.commitmate.re_cord.domain.post.post.dto.PostDTO;
+import com.commitmate.re_cord.domain.post.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/mypage")
+public class MypageController {
+
+    private final PostService postService;
+    private final CommentService commentService;
+
+
+    @GetMapping("/posts/{userId}")
+    public ResponseEntity<List<PostDTO>> getPostsByUser(@PathVariable("userId") Long userId) {
+        List<PostDTO> posts = postService.getPostsByUserId(userId);
+        return ResponseEntity.ok(posts);
+    }
+
+    @GetMapping("/comments/{userId}")
+    public ResponseEntity<List<CommentDTO>> getCommentsByUser(@PathVariable("userId") Long userId) {
+        List<CommentDTO> comments = commentService.getCommentsByUser(userId);
+        return ResponseEntity.ok(comments);
+    }
+}
+
+
+
+
