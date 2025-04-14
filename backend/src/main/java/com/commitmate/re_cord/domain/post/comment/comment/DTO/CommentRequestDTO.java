2978c29e7bca2325f57b7199a55d8a3e37c6cf9d
Youngheun <heeun.deco@gmail.com>
2025-04-14 14:14:12 +0900

    fix: dto 유효성 검사 어노테이션 수정

--- backend/src/main/java/com/commitmate/re_cord/domain/post/comment/comment/DTO/CommentRequestDTO.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/post/comment/comment/DTO/CommentRequestDTO.java
@@ -1,5 +1,7 @@
 package com.commitmate.re_cord.domain.post.comment.comment.DTO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentRequestDTO {
+    @NotBlank(message = "빈 댓글은 등록할 수 없습니다.")
+    @Size(max=200, message = "댓글은 최대 200자까지 등록 가능합니다.")
     private String content;
 
 }
