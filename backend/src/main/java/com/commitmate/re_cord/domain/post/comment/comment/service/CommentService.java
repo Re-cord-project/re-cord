9cb4d911fbca00bf3c5e7613a087d7e778a96823
asdf-qwe <fism@naver.com>
2025-04-14 18:22:51 +0900

    WIP: comment service 수정 중

--- backend/src/main/java/com/commitmate/re_cord/domain/post/comment/comment/service/CommentService.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/post/comment/comment/service/CommentService.java
@@ -1,54 +1,24 @@
-package com.commitmate.re_cord.domain.post.comment.comment.service;
+package com.commitmate.re_cord.domain.post.comment.comment.Service;
 
-
-import com.commitmate.re_cord.domain.post.comment.comment.DTO.CommentRequestDTO;
-import com.commitmate.re_cord.domain.post.comment.comment.DTO.CommentResponseDTO;
+import com.commitmate.re_cord.domain.post.comment.comment.Repository.CommentRepository;
+import com.commitmate.re_cord.domain.post.comment.comment.dto.CommentDTO;
 import com.commitmate.re_cord.domain.post.comment.comment.entity.Comment;
-import com.commitmate.re_cord.domain.post.comment.comment.repository.CommentRepository;
-import com.commitmate.re_cord.domain.post.post.entity.Post;
-import com.commitmate.re_cord.domain.post.post.repository.PostRepository;
-import com.commitmate.re_cord.domain.user.user.entity.User;
-import com.commitmate.re_cord.domain.user.user.repository.UserRepository;
-import com.commitmate.re_cord.global.jpa.UpdateStatus;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-@Slf4j
 public class CommentService {
     private final CommentRepository commentRepository;
-    private final PostRepository postRepository;
-    private final UserRepository userRepository;
-
-    @Transactional
-    public CommentResponseDTO registerComment(CommentRequestDTO commentRequestDTO, Long postId, Long userId){
-        Post post = postRepository.findById(postId)
-                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 존재하지 않습니다."));
-
-        User user = userRepository.findById(userId)
-                .orElseThrow(()-> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
-
-        Comment comment = new Comment(0,commentRequestDTO.getContent(), UpdateStatus.NOT_EDITED,user,post);
-        Comment savedComment = commentRepository.save(comment);
 
 
-        return new CommentResponseDTO(savedComment.getId(), savedComment.getContent(),user.getUsername(),savedComment.getCreatedAt().toString());
-    }
-
-    @Transactional
-    public void deleteComment(Long commentId, Long userId){
-
-        Comment comment = commentRepository.findById(commentId)
-                .orElseThrow(()-> new IllegalArgumentException("해당 댓글이 존재하지 않습니다."));
-
-        if(!comment.getUserId().getId().equals(userId)){
-            throw new IllegalArgumentException("댓글 작성자만 삭제할 수 있습니다.");
-        }
-
-        commentRepository.delete(comment);
-
+    public List<CommentDTO> getCommentsByUser(Long userId) {
+        return commentRepository.findMyComment(userId).stream()
+                .map(CommentDTO::getEntity)
+                .collect(Collectors.toList());
     }
 }
+
