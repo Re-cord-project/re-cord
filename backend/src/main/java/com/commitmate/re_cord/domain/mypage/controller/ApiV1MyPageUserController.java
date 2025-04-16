b648b40fb8d41b910e3c040b3921c889539f7e06
codefish <codewithfish@gmail.com>
2025-04-16 17:55:03 +0900

    feat:회원정보 수정하기

---
+++ backend/src/main/java/com/commitmate/re_cord/domain/mypage/controller/ApiV1MyPageUserController.java
@@ -0,0 +1,25 @@
+package com.commitmate.re_cord.domain.mypage.controller;
+
+
+import com.commitmate.re_cord.domain.mypage.service.MyPageUserService;
+import com.commitmate.re_cord.domain.user.user.dto.UpdateUserDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mypage")
+@RequiredArgsConstructor
+public class ApiV1MyPageUserController {
+
+    private final MyPageUserService myPageUserService;
+
+    @PutMapping("/{userId}/userUpdate")
+    public ResponseEntity<UpdateUserDTO> updateUser(@PathVariable("userId") Long userId, @RequestBody UpdateUserDTO updateUserDTO) {
+        UpdateUserDTO updatedUser = myPageUserService.updateUser(userId, updateUserDTO);
+        return ResponseEntity.ok(updatedUser);
+    }
+}
