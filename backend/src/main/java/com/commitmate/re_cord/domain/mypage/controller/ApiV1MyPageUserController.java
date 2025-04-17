efb917d994c9a45e9bf7f2d7855fbaaac334f13f
codefish <codewithfish@gmail.com>
2025-04-17 18:48:30 +0900

    feat:JWT 로그인 인증 변경

--- backend/src/main/java/com/commitmate/re_cord/domain/mypage/controller/ApiV1MyPageUserController.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/mypage/controller/ApiV1MyPageUserController.java
@@ -3,8 +3,10 @@ package com.commitmate.re_cord.domain.mypage.controller;
 
 import com.commitmate.re_cord.domain.mypage.service.MyPageUserService;
 import com.commitmate.re_cord.domain.user.user.dto.UpdateUserDTO;
+import com.commitmate.re_cord.global.security.SecurityUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,11 @@ public class ApiV1MyPageUserController {
     private final MyPageUserService myPageUserService;
 
     //유저의 세부 정보 변경
-    @PutMapping("/{userId}/userUpdate")
-    public ResponseEntity<UpdateUserDTO> updateUser(@PathVariable("userId") Long userId, @RequestBody UpdateUserDTO updateUserDTO) {
+    @PutMapping("/userUpdate")
+    public ResponseEntity<UpdateUserDTO> updateUser(
+            @AuthenticationPrincipal SecurityUser userDetails,
+        @RequestBody UpdateUserDTO updateUserDTO) {
+        Long userId = userDetails.getId();
         UpdateUserDTO updatedUser = myPageUserService.updateUser(userId, updateUserDTO);
         return ResponseEntity.ok(updatedUser);
     }
