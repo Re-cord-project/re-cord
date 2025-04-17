ef8f9577451bbd9db28442d5fd34c8f52dd1825e
Youngheun <heeun.deco@gmail.com>
2025-04-17 14:42:33 +0900

    feat:댓글 조회 기능 구현

--- backend/src/main/java/com/commitmate/re_cord/domain/post/comment/comment/service/CommentService.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/post/comment/comment/service/CommentService.java
@@ -13,8 +13,14 @@ import com.commitmate.re_cord.domain.user.user.repository.UserRepository;
 import com.commitmate.re_cord.global.jpa.UpdateStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.domain.Pageable;
+
+
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -71,6 +77,19 @@ public class CommentService {
 
         Comment updatedComment = commentRepository.save(comment);
         return new CommentResponseDTO(updatedComment.getId(),updatedComment.getContent(),updatedComment.getUser().getUsername(),updatedComment.getCreatedAt().toString());
+    }
+
+    @Transactional
+    public Page<CommentResponseDTO> getCommentByPostId(Long postId, int page, int size){
+        Post post = postRepository.findById(postId)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시물이 존재하지 않습니다."));
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
+
+        Page<Comment> commentPage = commentRepository.findByPostId(postId,pageable);
+
+
+        return commentPage.map(CommentResponseDTO::new);
     }
 
     public List<CommentDTO> getCommentsByUser(Long userId) {
