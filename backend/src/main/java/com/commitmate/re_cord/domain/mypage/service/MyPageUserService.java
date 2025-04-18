e95e37299620e67771b2126fbf9dac78eed8acc3
kunhr <kunhr@naver.com>
2025-04-18 11:40:53 +0900

    fix: nickname 메서드 주석처리

--- backend/src/main/java/com/commitmate/re_cord/domain/mypage/service/MyPageUserService.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/mypage/service/MyPageUserService.java
@@ -20,9 +20,9 @@ public class MyPageUserService {
                 .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
 
         // 닉네임 중복 체크
-        if (userRepository.existsByNicknameAndIdNot(dto.getNickname(), userId)) {
-            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
-        }
+//        if (userRepository.existsByNicknameAndIdNot(dto.getNickname(), userId)) {
+//            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+//        }
 
         // 이메일 중복 체크
         if (userRepository.existsByEmailAndIdNot(dto.getEmail(), userId)) {
@@ -30,7 +30,7 @@ public class MyPageUserService {
         }
 
         // 업데이트할 값 설정
-        user.setNickname(dto.getNickname());
+//        user.setNickname(dto.getNickname());
         user.setEmail(dto.getEmail());
         user.setGeneration(dto.getGeneration());
         user.setBootcamp(dto.getBootcamp());
