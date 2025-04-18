0cec386fccd0223ec53a3cadba11ceee5c57f8cb
Youngheun <heeun.deco@gmail.com>
2025-04-18 12:10:42 +0900

    fix:Comment API URL 수정

--- backend/src/main/java/com/commitmate/re_cord/domain/post/comment/comment/controller/ApiV1CommentController.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/post/comment/comment/controller/ApiV1CommentController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/{postId}/comments")
+@RequestMapping("api/{postId}/comments")
 @RequiredArgsConstructor
 public class ApiV1CommentController {
 
