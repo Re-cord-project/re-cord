b394eedbe75f752d6b6c1f32c554279d880684ef
codefish <codewithfish@gmail.com>
2025-04-22 10:44:06 +0900

    fix:성능 최적화

--- backend/src/main/java/com/commitmate/re_cord/domain/mypage/service/MyPagePostService.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/mypage/service/MyPagePostService.java
@@ -6,6 +6,7 @@ import com.commitmate.re_cord.domain.post.post.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -13,6 +14,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@Transactional(readOnly = true)
 
 public class MyPagePostService {
     private final PostRepository postRepository;
