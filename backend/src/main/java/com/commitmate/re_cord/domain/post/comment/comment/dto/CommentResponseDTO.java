8c2df96ad26cde379ba2b7c5544702c0a4a0ceaf
Youngheun <heeun.deco@gmail.com>
2025-04-24 00:20:24 +0900

    feat:대댓글 기능 구현

--- backend/src/main/java/com/commitmate/re_cord/domain/post/comment/comment/dto/CommentResponseDTO.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/post/comment/comment/dto/CommentResponseDTO.java
@@ -1,31 +1,13 @@
 package com.commitmate.re_cord.domain.post.comment.comment.dto;
 
 import com.commitmate.re_cord.domain.post.comment.comment.entity.Comment;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import java.time.LocalDateTime;
-
-//@Getter
-//@AllArgsConstructor
-//@NoArgsConstructor
-//public class CommentResponseDTO {
-//    private Long id;
-//    private String content;
-//    private String username;
-//    private String createdAt;
-//
-//    public CommentResponseDTO(Comment comment) {
-//        this.id = comment.getId();
-//        this.content = comment.getContent();
-//        this.username = comment.getUser().getUsername();
-//        this.createdAt = comment.getCreatedAt().toString();
-//    }
-//}
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CommentResponseDTO {
@@ -36,14 +18,18 @@ public class CommentResponseDTO {
     private String updateStatus;
     private String profileImageUrl;
     private int likes;
+    private Long parentId;
+    private List<CommentResponseDTO> replies;
 
     public CommentResponseDTO(Comment comment) {
         this.id = comment.getId();
-        this.content = comment.getContent();
+        this.content = comment.isDeleted() ? "삭제된 댓글입니다." : comment.getContent(); //Soft Delete
         this.username = comment.getUser().getUsername();
         this.createdAt = comment.getCreatedAt().toString();
         this.updateStatus = comment.getUpdateStatus().name();
         this.profileImageUrl = comment.getUser().getProfileImageUrl();
         this.likes = comment.getLikes();
+        this.parentId = comment.getParent() != null ? comment.getParent().getId() : null;
+        this.replies = new ArrayList<>();
     }
 }
