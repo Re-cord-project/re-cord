0753223f1485b5f57b889add9e35e343508c818a
codefish <codewithfish@gmail.com>
2025-04-24 23:32:38 +0900

    feat:내정보열람및수정

--- backend/src/main/java/com/commitmate/re_cord/domain/mypage/service/MyPageUserService.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/mypage/service/MyPageUserService.java
@@ -15,35 +15,46 @@ public class MyPageUserService {
 
     private final UserRepository userRepository;
 
+
+    @Transactional(readOnly = true)
+    public UpdateUserDTO getUserInfo(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
+
+        return UpdateUserDTO.builder()
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .bootcamp(user.getBootcamp())
+                .generation(user.getGeneration())
+                .introduction(user.getIntroduction())
+                .profileImageUrl(user.getProfileImageUrl())
+                .build();
+    }
+
+
+
     @Transactional
     public UpdateUserDTO updateUser(Long userId, UpdateUserDTO dto) {
 
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("유저를 찾을 수 없습니다."));
-
         // 닉네임 중복 체크
-
         if (userRepository.existsByUsernameAndIdNot(dto.getUsername(), userId)) {
             throw new IllegalArgumentException("이미 사용 중인 이름입니다.");
         }
-
-
         // 이메일 중복 체크
         if (userRepository.existsByEmailAndIdNot(dto.getEmail(), userId)) {
             throw new IllegalArgumentException("이미 사용 중인 이메일입니다.");
         }
-
         // 업데이트할 값 설정
-
         user.setUsername(dto.getUsername());
-
         user.setEmail(dto.getEmail());
         user.setGeneration(dto.getGeneration());
         user.setBootcamp(dto.getBootcamp());
-
+        user.setIntroduction(dto.getIntroduction());
+        user.setProfileImageUrl(dto.getProfileImageUrl()); // profileImageUrl 추가
         // 유저 정보 저장
         userRepository.save(user);
-
         return dto;
     }
 }
