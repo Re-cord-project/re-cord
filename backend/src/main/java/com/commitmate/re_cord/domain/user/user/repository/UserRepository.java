b66b726dce454093b1e211dc1531b42cfd98cf07
asdf-qwe <fism@naver.com>
2025-04-30 01:24:35 +0900

    카테고리 FIX

--- backend/src/main/java/com/commitmate/re_cord/domain/user/user/repository/UserRepository.java
+++ backend/src/main/java/com/commitmate/re_cord/domain/user/user/repository/UserRepository.java
@@ -3,8 +3,11 @@ package com.commitmate.re_cord.domain.user.user.repository;
 
 import com.commitmate.re_cord.domain.user.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -27,5 +30,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByBlogName(String blogName);
 
     Optional<User> getBootcampById(Long id);
+
+    @Query("""
+        SELECT u FROM User u
+        JOIN u.categories c
+        WHERE c.id = :categoryId
+    """)
+    List<User> findUsersByCategoryId(@Param("categoryId") Long categoryId);
 }
 
