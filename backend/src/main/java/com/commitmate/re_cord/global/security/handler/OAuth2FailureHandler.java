c6d880178256318d88e976e7cd4950b08e3f9e17
kunhr <kunhr@naver.com>
2025-04-30 23:02:27 +0900

    수정

--- backend/src/main/java/com/commitmate/re_cord/global/security/handler/OAuth2FailureHandler.java
+++ backend/src/main/java/com/commitmate/re_cord/global/security/handler/OAuth2FailureHandler.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 @Component
 public class OAuth2FailureHandler implements AuthenticationFailureHandler {
 
-    @Value("https://www.re-cord.site/home")
+    @Value("https://www.re-cord.site")
     private String frontendUrl;
 
     @Override
