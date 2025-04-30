c9253b50bc8f0d02937b0bbb8f88212812a7a5fc
codefish <codewithfish@gmail.com>
2025-04-30 17:19:26 +0900

    main.tf 수정

--- infra/main.tf
+++ infra/main.tf
@@ -178,8 +178,10 @@ resource "aws_iam_instance_profile" "instance_profile_1" {
 
 locals {
   ec2_user_data_base = <<-END_OF_FILE
+
 #!/bin/bash
 # 가상 메모리 4GB 설정
+
 sudo dd if=/dev/zero of=/swapfile bs=128M count=32
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
@@ -206,6 +208,63 @@ docker run -d \
   -v /dockerProjects/npm_1/volumes/data:/data \
   -v /dockerProjects/npm_1/volumes/etc/letsencrypt:/etc/letsencrypt \
   jc21/nginx-proxy-manager:latest
+
+# ha proxy 설치
+## 설정파일을 위한 디렉토리 생성
+mkdir -p /dockerProjects/ha_proxy_1/volumes/usr/local/etc/haproxy/lua
+
+cat << 'EOF' > /dockerProjects/ha_proxy_1/volumes/usr/local/etc/haproxy/lua/retry_on_502_504.lua
+core.register_action("retry_on_502_504", { "http-res" }, function(txn)
+  local status = txn.sf:status()
+  if status == 502 or status == 504 then
+    txn:Done()
+  end
+end)
+EOF
+
+## 설정파일 생성
+echo -e "
+global
+    lua-load /usr/local/etc/haproxy/lua/retry_on_502_504.lua
+
+resolvers docker
+    nameserver dns1 127.0.0.11:53
+    resolve_retries       3
+    timeout retry         1s
+    hold valid            10s
+
+defaults
+    mode http
+    timeout connect 5s
+    timeout client 60s
+    timeout server 60s
+
+## api.blog.sik2.site => 프로젝트 API 서버 도메인
+frontend http_front
+    bind *:80
+    acl host_app1 hdr_beg(host) -i api.blog.re-cord.site
+
+    use_backend http_back_1 if host_app1
+
+backend http_back_1
+    balance roundrobin
+    option httpchk GET /actuator/health
+    default-server inter 2s rise 1 fall 1 init-addr last,libc,none resolvers docker
+    option redispatch
+    http-response lua.retry_on_502_504
+
+    server app_server_1_1 app1_1:8080 check
+    server app_server_1_2 app1_2:8080 check
+" > /dockerProjects/ha_proxy_1/volumes/usr/local/etc/haproxy/haproxy.cfg
+
+docker run \
+  -d \
+  --network common \
+  -p 8090:80 \
+  -v /dockerProjects/ha_proxy_1/volumes/usr/local/etc/haproxy:/usr/local/etc/haproxy \
+  -e TZ=Asia/Seoul \
+  --name ha_proxy_1 \
+  haproxy
 
 # redis 설치
 docker run -d \
